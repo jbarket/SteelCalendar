8e1de5f7724769ff5b032c54bce7ba14f89c5d35
Jonathan Barket <jbarket@sleepunit.com>
2013-07-05 21:47:38 +0000

    minor tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,12 +23,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
+    redirect_to settings_user_path(current_user)
   end
 
   # GET /users/new
@@ -54,7 +49,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to settings_user_path(@user), notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
