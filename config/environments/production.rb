f92125a68dc50ab8defe08fc0188bbecdabbed73
Jonathan Barket <jbarket@sleepunit.com>
2013-07-16 19:20:08 +0000

    trying the host thing again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Steelcalendar::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-config.action_mailer.default_url_options = { :host => 'http://www.steelcalendar.com' }
+config.action_mailer.default_url_options = { :host => 'www.steelcalendar.com' }
 
   config.paperclip_defaults = {
     :storage => :s3,
