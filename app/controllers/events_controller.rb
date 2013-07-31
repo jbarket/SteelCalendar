2d2846ae1d599c998b0901818b93ba98f0e453e3
Jonathan Barket <jbarket@sleepunit.com>
2013-07-31 20:22:55 +0000

    trying to add zipcode searching

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,8 +17,8 @@ class EventsController < ApplicationController
 
     unless params[:distance] == "All"
 
-      if !params[:latitude].blank? and !params[:longitude].blank?
-        @events = @events.near([params[:latitude], params[:longitude]], params[:distance]) | @events.where(["major_convention = ? AND major_convention_approved IS NOT NULL", true])
+      if !params[:zipcode].blank?
+        @events = @events.near(params[:zipcode], params[:distance]) | @events.where(["major_convention = ? AND major_convention_approved IS NOT NULL", true])
       elsif request.location
         @events = @events.near([request.location.latitude, request.location.longitude], params[:distance]) | @events.where(["major_convention = ? AND major_convention_approved IS NOT NULL", true])
       end
